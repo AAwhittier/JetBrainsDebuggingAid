f1ee716b801392e981bf6006619f9875257b314d
GitHub Action <action@github.com>
2021-05-25 17:36:32 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sirwranwrap/jetbrainsdebuggingaid/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sirwranwrap.jetbrainsdebuggingaid.services
+
+import com.github.sirwranwrap.jetbrainsdebuggingaid.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/sirwranwrap/jetbrainsdebuggingaid/services/MyProjectService.kt
+++ src/main/kotlin/com/github/sirwranwrap/jetbrainsdebuggingaid/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.sirwranwrap.jetbrainsdebuggingaid.services
 
+import com.github.sirwranwrap.jetbrainsdebuggingaid.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
